3230943ad5d131c47a971d2ba3d3964577d05741
skyria10 <60612406+skyria10@users.noreply.github.com>
2021-10-06 12:39:50 +0300

    setup of graphql with react

--- parkpick/user-app/src/main/java/com/parkpick/userapp/UserAppApplication.java
+++ parkpick/user-app/src/main/java/com/parkpick/userapp/UserAppApplication.java
@@ -1,21 +1,24 @@
-package com.parkpick.userapp;
+package com.ParkPick.userapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
-public class UserAppApplication implements CommandLineRunner{
+public class UserAppApplication implements CommandLineRunner {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserAppApplication.class, args);
+
 	}
 
 	@Override
