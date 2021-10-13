dc2064ed20f923fee11ddf7709d9861414f633f3
Stavros Spyrou <sspyro03@ucy.ac.cy>
2021-10-13 12:51:51 +0300

    Testing

--- client/user/src/App.js
+++ client/user/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test test test
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/stavross.spy"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TESTING
         </a>
       </header>
     </div>
