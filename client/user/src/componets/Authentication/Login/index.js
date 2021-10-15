2624c9361c9765285027c1edb2de119945edffeb
lpapal03 <lpapal03@ucy.ac.cy>
2021-10-15 18:36:40 +0300

    Update index.js

--- client/user/src/componets/Authentication/Login/index.js
+++ client/user/src/componets/Authentication/Login/index.js
@@ -1,15 +1,5 @@
-import { 
-    Text,
-    Box, 
-    Heading, 
-    Button, 
-    Stack,
-    Icon,
-    } from '@chakra-ui/react';
-import {AiFillCar} from 'react-icons/ai'
+import { Text, Box, Heading, Button, Stack, Icon } from "@chakra-ui/react";
+import { AiFillCar } from "react-icons/ai";
 export default function LogIn() {
-    return(
-        <Text>En leitourgei</Text>
-    );
+  return <Text>En leitourgei</Text>;
 }
-
