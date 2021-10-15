2d922bb934aa7b8180e8bb88c82cf09e11e38903
stytSofo <ssofok02@ucy.ac.cy>
2021-10-15 20:23:49 +0300

    space no change

--- client/user/src/componets/LandingPage/index.js
+++ client/user/src/componets/LandingPage/index.js
@@ -1,43 +1,40 @@
-import { 
-    Text,
-    Box, 
-    Heading, 
-    Button, 
-    Stack,
-    Icon,
-    } from '@chakra-ui/react';
-import {FaCar} from 'react-icons/fa'
-import LogIn from '../Authentication/Login';
+import { Text, Box, Heading, Button, Stack, Icon } from "@chakra-ui/react";
+import { FaCar } from "react-icons/fa";
+import LogIn from "../Authentication/Login";
 
 export default function LandingPage() {
-     const login = LogIn();
-    return(
-        <Stack 
-            bgColor='#16DABF'
-            alignContent='center' 
-            textAlign='centre' 
-            display='flex' 
-            flexDirection='column' 
-            boxSize='3xl'
-            blockSize='full'
-            width='full'
-            height='full'
-
-        >
-            <Box alignSelf='center'>
-                <Icon alignItems='center' as={FaCar} boxSize='3xs' alignContent='center'/>
-            </Box>
-            
-            <Stack width='52'alignSelf='center'>
-                {/* <Button color='blue.100' >landing page </Button> */}
-                <Button onClick={login}>LOG IN </Button>
-                <Button>SIGN UP </Button>
-                <Button>ONE TIME PAYMENT </Button>
-            </Stack>
-            /* by inceasing the size of a box, the outer stack gets bigger
-            and covers the screen */
-            <Box bgColor="#16DABF" width="10" height="700"></Box> 
+  const login = LogIn();
+  return (
+    
+      <Stack
+        bgColor="#16DABF"
+        alignContent="center"
+        textAlign="centre"
+        display="flex"
+        flexDirection="column"
+        boxSize="3xl"
+        blockSize="full"
+        width="full"
+        height="full"
+      >
+        <Box alignSelf="center">
+          <Icon
+            alignItems="center"
+            as={FaCar}
+            boxSize="3xs"
+            alignContent="center"
+          />
+        </Box>
+        <Stack width="52" alignSelf="center">
+          {/* <Button color='blue.100' >landing page </Button> */}
+          <Button onClick={login}>LOG IN </Button>
+          <Button>SIGN UP </Button>
+          <Button>ONE TIME PAYMENT </Button>
         </Stack>
-    );
+        /* by inceasing the size of a box, the outer stack gets bigger and
+        covers the screen */
+        <Box bgColor="#16DABF" width="10" height="700"></Box>
+      </Stack>
+    
+  );
 }
-
