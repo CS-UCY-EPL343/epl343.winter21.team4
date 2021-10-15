b6f54a8366bd6b60f30ad12ae7f2dbac5cabad76
stytSofo <ssofok02@ucy.ac.cy>
2021-10-15 20:28:29 +0300

    😥😥😥😥

--- client/user/src/componets/Authentication/Login/index.js
+++ client/user/src/componets/Authentication/Login/index.js
@@ -1,5 +1,6 @@
 import { Text, Box, Heading, Button, Stack, Icon } from "@chakra-ui/react";
 import { AiFillCar } from "react-icons/ai";
+  
 export default function LogIn() {
   return <Text>En leitourgei</Text>;
 }
