d12c32bf99ea0314ee1849e7f996d7595ad437e6
Stavros Spyrou <sspyro03@ucy.ac.cy>
2021-10-20 15:40:20 +0300

    Update Login Page

--- client/user/src/componets/Authentication/Login/index.js
+++ client/user/src/componets/Authentication/Login/index.js
@@ -1,13 +1,50 @@
-import { Text, Box, Heading, Button, Stack, Icon, Input } from "@chakra-ui/react";
-import { AiFillCar } from "react-icons/ai";
-  
+import { Text, Box, Heading, Button, Stack, Icon } from "@chakra-ui/react";
+import { FaCar } from "react-icons/fa";
 export default function LogIn() {
   return (
-    <Stack>
-<Input variant="flushed" placeholder="Telephone" bgColor="black"></Input>
 
-<Input></Input>
+    <Stack
+        bgColor="#16DABF"
+        alignContent="center"
+        textAlign="centre"
+        display="flex"
+        flexDirection="column"
+        boxSize="3xl"
+        blockSize="full"
+        width="full"
+        height="full"
+        minHeight="full"
+        minWidth="full"
+    >
 
+         <Box alignSelf="center">
+          <Icon
+            alignItems="center"
+            as={FaCar}
+            boxSize="3xs"
+            alignContent="center"
+          />
+        </Box>
+
+    <Stack width="52" alignSelf="center">
+<Input  pr="4.5rem" variant="filled" placeholder="Email"></Input>
+
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="Password"
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+ 
+
+
+<Button>LOG IN</Button>
+
+    </Stack>
     </Stack>
    
    )
