efaede78d0ed8111dd1b8f32d395f0aa1607a0b9
lpapal03 <lpapal03@ucy.ac.cy>
2021-11-07 10:46:28 +0200

    Typo fix

--- client/user/src/componets/Authentication/SignUp/index.js
+++ client/user/src/componets/Authentication/SignUp/index.js
@@ -41,7 +41,7 @@ export default function SignUp() {
         color="black"
         paddingBottom="5"
       >
-        <Text>Craete a new account</Text>
+        <Text>Create a new account</Text>
       </Heading>
 
       <Stack spacing={3} alignSelf="center">
