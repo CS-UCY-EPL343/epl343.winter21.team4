fe34bfcae65cd32a79632b731dc630cc2d172e1c
stytSofo <ssofok02@ucy.ac.cy>
2021-11-18 16:23:11 +0200

    Search Functionality

--- client/user/src/componets/Views/Search/index.js
+++ client/user/src/componets/Views/Search/index.js
@@ -15,8 +15,16 @@ import {
   formState,
   form,
   Link,
+  NumberInput,
+  NumberInputField,
+  IconButton,
+  Image,
 } from "@chakra-ui/react";
 import { FaCar } from "react-icons/fa";
+import { SearchIcon } from "@chakra-ui/icons";
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactSearchBox from "react-search-box";
 import {
   BrowserRouter,
   Route,
@@ -24,10 +32,61 @@ import {
   Link as RouterLink,
 } from "react-router-dom";
 import LandingPage from "../../LandingPage";
-import { facar } from "react-icons/fa";
-import Layout from "../NavigationBar/Layout";
+import { useState } from "react";
+import { SearchBox } from "react-instantsearch-dom";
+import Data from "./MOCK_DATA.json";
 
 export default function Search() {
-  return (<Text>Kokos</Text>)
-  
+  const [symbolsArr] = useState(["e", "E", "+", "-", "."]);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Box alignSelf="center">
+      <Link as={RouterLink} to="/profile">
+        <Button width="full">GO TO PROFILE</Button>
+      </Link>
+
+      <Stack>
+        <Box mx={{ sm: "auto" }} w={{ sm: "13" }} maxW={{ sm: "md" }}>
+          <NumberInput
+            onChange={(valueString) => setValue(valueString)}
+            value={value}
+            max={9999}
+            clampValueOnBlur={false}
+            isRequired={true}
+            inputMode="numeric"
+            onKeyDown={(e) => symbolsArr.includes(e.key) && e.preventDefault()}
+          >
+            <IconButton
+              colorScheme="blue"
+              aria-label="Search database"
+              icon={<SearchIcon />}
+              type="submit"
+            />
+            <NumberInputField
+              placeholder="#"
+              maxLength="4"
+              mx={{ sm: "auto" }}
+              w={{ sm: "13" }}
+              maxW={{ sm: "md" }}
+              id="searchBox"
+              name="box"
+            ></NumberInputField>
+          </NumberInput>
+          {Data.filter((parking) => {
+            if (value === "") {
+              return null;
+            } else if (parking.id == value) {
+              return parking;
+            }
+          }).map((parking, index) => (
+            <div key={index}>
+              <Text>{parking.first_name}</Text>
+              <img src={parking.picture} />
+            </div>
+          ))}
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
