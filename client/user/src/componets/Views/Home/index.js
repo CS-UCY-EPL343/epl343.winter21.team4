0ad60bdeb6584f9baae37e52e46c0d3778892a53
lpapal03 <lpapal03@ucy.ac.cy>
2021-11-21 13:00:39 +0200

    created homepage

--- client/user/src/componets/Views/Home/index.js
+++ client/user/src/componets/Views/Home/index.js
@@ -48,25 +48,34 @@ export default function Home(url) {
           <Box>aaa</Box>
         </Box> */}
 
-        {/* <Box bg="#16DABF" w="100%" p={4} color="white" borderRadius="md">
-          <Grid templateColumns="repeat(5, 1fr)" gap={10}>
-            <Box w="40%" p={4} borderRadius="md" fontSize="2xl">
-              The easiest way to park.
-            </Box>
-          </Grid>
-        </Box> */}
-
-        <Grid
-          h="200px"
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(5, 1fr)"
-          gap={4}
+        <Box
+          bg="#16DABF"
+          w="100%"
+          p={4}
+          color="white"
+          borderRadius="md"
+          h="500"
         >
-          <GridItem rowSpan={2} colSpan={1} bg="tomato" />
-          <GridItem colSpan={2} bg="papayawhip" />
-          <GridItem colSpan={2} bg="papayawhip" />
-          <GridItem colSpan={4} bg="tomato" />
-        </Grid>
+          <Grid
+            gap={1}
+            templateRows="repeat(1, 1fr)"
+            templateColumns="repeat(2, 1fr)"
+          >
+            <GridItem rowSpan={1} colSpan={1} w="200px" fontSize="6xl" p={1}>
+              The easiest way to park.
+            </GridItem>
+            <GridItem rowSpan={1} colSpan={1} w="300px" fontSize="6xl" p={1}>
+              <Box boxSize="sm">
+                <Image src="https://i.imgur.com/ih8xulQ.png" alt="alt" />
+              </Box>
+            </GridItem>
+          </Grid>
+          <Link as={RouterLink} to="/search">
+            <Button colorScheme="teal" variant="outline">
+              Get Started
+            </Button>
+          </Link>
+        </Box>
       </Heading>
       <Stack paddingTop="5"></Stack>
     </Stack>
