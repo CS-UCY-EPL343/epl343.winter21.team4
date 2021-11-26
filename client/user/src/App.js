d662fdb00660089e570f5f0b8a71c4e4da5478d5
Stavros Spyrou <sspyro03@ucy.ac.cy>
2021-11-26 15:17:25 +0200

    theme creation

--- client/user/src/App.js
+++ client/user/src/App.js
@@ -2,10 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Text, Box, Heading, ChakraProvider } from "@chakra-ui/react";
 import Main from "./componets/Main";
+import theme from "./theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Main />
     </ChakraProvider>
   );
---
+++ client/user/src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+const theme = extendTheme({
+  colors: {
+    brand: {
+      green: "#16DABF",
+      teal: "#81E6D9",
+      white: "#FFFFFF",
+      yellow: "#F6E05E",
+    },
+  },
+});
+
+export default theme;
