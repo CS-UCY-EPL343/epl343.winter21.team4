44d8eee0439341a727a9ee88fbd52b6f4037ca6c
garist03 <62186755+garist03@users.noreply.github.com>
2021-11-27 01:10:56 +0200

    Added styling so the form appears in a box

--- client/user/src/componets/Views/Card/AddNewCard/useForm.js
+++ client/user/src/componets/Views/Card/AddNewCard/useForm.js
@@ -1,1 +1,39 @@
-const useForm = () => {};
+import { useState } from "react";
+import validateInfo from "./validateInfo";
+
+const useForm = () => {
+  const [values, setValues] = useState({
+    cardName: "",
+    cardNumber: "",
+    cardType: "",
+    cardExpiration: "",
+    cardSecurityCode: "",
+    focus: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleFocus = (e) => {
+    setValues({
+      ...values,
+      focus: e.target.name === "cardSecurityCode" ? "cvc" : e.target.name,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateInfo(values));
+  };
+
+  return { handleChange, handleFocus, handleSubmit, values, errors };
+};
+
+export default useForm;
--- client/user/src/componets/Views/Card/AddNewCard/validateInfo.js
+++ client/user/src/componets/Views/Card/AddNewCard/validateInfo.js
@@ -2,6 +2,12 @@ import valid from "card-validator";
 
 export default function validateInfo(values) {
   let errors = {};
+  let creditCard = valid.number(values.number);
+  creditCard.expirationDate = valid.expirationDate(values.expiration);
+  creditCard.cardholderName = valid.cardholderName(values.name);
+  creditCard.cvc = valid.cvc(values.ccvc);
+
+  console.log(creditCard);
 
   errors.show = true;
   errors.variant = "danger";
@@ -10,6 +16,47 @@ export default function validateInfo(values) {
   errors.cnumber = false;
   errors.cexp = false;
   errors.ccvc = false;
+
+  //Card CVC Error Messages and checks
+  if (values.cvc === null || !values.cvc.trim()) {
+    errors.message = "Credit Card CVC is not complete";
+  } else if (creditCard.cvc.isValid) {
+    errors.ccvc = true;
+  } else {
+    errors.message = "Credit Card CVC is invalid";
+  }
+
+  //Card Expiration Date Error Messages and checks
+  if (values.expiration === null || !values.expiration.trim()) {
+    errors.message = "Credit Card Expiration Date is not complete";
+  } else if (creditCard.expirationDate.isValid) {
+    errors.cexp = true;
+  } else {
+    errors.message = "Credit Card Expiration Date is invalid";
+  }
+
+  //Card Number Error Messages and checks
+  if (values.number === null || !values.number.trim()) {
+    errors.message = "Credit Card Number is not complete";
+  } else if (creditCard.number.isValid) {
+    errors.cnumber = true;
+  } else {
+    errors.message = "Credit Card Number is invalid";
+  }
+
+  //Card Name Error Messages and checks
+  if (values.name === null || !values.name.trim()) {
+    errors.message = "Credit Card Name is not complete";
+  } else if (creditCard.name.isValid) {
+    errors.cname = true;
+  } else {
+    errors.message = "Credit Card Name is invalid";
+  }
+
+  if (errors.cname && errors.cnumber && errors.ccvc && errors.cexp) {
+    errors.variant = "Success!";
+    errors.message = "Credit Card is Valid!";
+  }
 
   return errors;
 }
