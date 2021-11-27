1812782c36791a7b9712943ae8bda4cc86f10fcb
Stavros Spyrou <sspyro03@ucy.ac.cy>
2021-11-27 12:32:18 +0200

    completed choose payment screen

--- client/user/src/componets/Views/ChoosePayment/index.js
+++ client/user/src/componets/Views/ChoosePayment/index.js
@@ -31,78 +31,83 @@ export default function ChoosePaymentMethod({ url }) {
   const { pathname } = useLocation();
   return (
     <Stack width="-webkit-fill-available">
-      <Heading paddingTop="10" alignSelf="center" size="l">
-        Choose Payment Method
-      </Heading>
-      <Stack width="min-content" alignSelf="center">
-        <Button size="xl" borderRadius="15" backgroundColor="gray.300">
-          <HStack
-            paddingTop="3"
-            paddingBottom="3"
-            paddingLeft="3"
-            paddingRight="3"
-          >
-            <FaCcMastercard size="50" paddingLeft="5" />
-            <Text>
-              **** **** **** 1234
-              {<br />}
-              Sophoclis Kyriacou
-              {<br />}
-              Mastercard
-            </Text>
-          </HStack>
-        </Button>
-        <Button size="xl" backgroundColor="gray.300" borderRadius="15">
-          <HStack
-            paddingTop="3"
-            paddingBottom="3"
-            paddingLeft="3"
-            paddingRight="3"
-          >
-            <FaCcVisa size="50" paddingLeft="5" />
-            <Text>
-              **** **** **** 1234
-              {<br />}
-              Stylianos Sofokleous
-              {<br />}
-              Visa
-            </Text>
-          </HStack>
-        </Button>
-
-        <HStack paddingTop="85" alignSelf="center">
-          <Text color="gray.500">Kokos Parking</Text>
-          <Text color="gray.500" paddingLeft="70">
-            €3.00
-          </Text>
-        </HStack>
-        <hr style={{ backgroundColor: "gray", height: "1px" }} />
-        <HStack alignSelf="center">
-          <Text color="black" size="lg">
-            TOTAL DUE:
-          </Text>
-          <Text color="black" paddingLeft="70" size="lg">
-            €3.00
-          </Text>
-        </HStack>
-
-        <Link
-          as={RouterLink}
-          to={`${url}/thankyou`}
-          alignSelf="center"
-          width="100%"
-        >
-          <Button
-            size="lg"
-            backgroundColor="gray.300"
-            borderRadius="15"
-            width="100%"
-            textAlign="center"
-            bgColor="#16DABF"
-          >
-            <Text>PAY €3.00</Text>
+      <Stack alignSelf="center">
+        <Heading paddingTop="10" alignSelf="center" size="l">
+          Choose Payment Method
+        </Heading>
+        <Stack width="min-content" alignSelf="center">
+          <Button size="xl" borderRadius="15" backgroundColor="gray.300">
+            <HStack
+              paddingTop="3"
+              paddingBottom="3"
+              paddingLeft="3"
+              paddingRight="3"
+            >
+              <FaCcMastercard size="50" paddingLeft="5" />
+              <Text>
+                **** **** **** 1234
+                {<br />}
+                Sophoclis Kyriacou
+                {<br />}
+                Mastercard
+              </Text>
+            </HStack>
           </Button>
-        </Link>
+          <Button size="xl" backgroundColor="gray.300" borderRadius="15">
+            <HStack
+              paddingTop="3"
+              paddingBottom="3"
+              paddingLeft="3"
+              paddingRight="3"
+            >
+              <FaCcVisa size="50" paddingLeft="5" />
+              <Text>
+                **** **** **** 1234
+                {<br />}
+                Stylianos Sofokleous
+                {<br />}
+                Visa
+              </Text>
+            </HStack>
+          </Button>
+        </Stack>
+        <Stack position="absolute" bottom="0" paddingBottom="2">
+          <HStack alignSelf="center">
+            <Text color="gray.500" textAlign="left">
+              Kokos Parking
+            </Text>
+            <Text color="gray.500" paddingLeft="70">
+              €3.00
+            </Text>
+          </HStack>
+          <hr style={{ backgroundColor: "gray", height: "1px" }} />
+          <HStack alignSelf="center">
+            <Text color="black" size="lg" textAlign="left">
+              TOTAL DUE:
+            </Text>
+            <Text color="black" paddingLeft="75" size="lg">
+              €3.00
+            </Text>
+          </HStack>
+
+          <Link
+            as={RouterLink}
+            to={`${url}/thankyou`}
+            alignSelf="center"
+            width="100%"
+          >
+            <Button
+              size="lg"
+              backgroundColor="gray.300"
+              borderRadius="15"
+              width="100%"
+              textAlign="center"
+              bgColor="#16DABF"
+            >
+              <Text>PAY €3.00</Text>
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
     </Stack>
   );
