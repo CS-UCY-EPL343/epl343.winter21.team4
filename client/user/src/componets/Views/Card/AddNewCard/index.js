713e2f539381dd6209b4dd1ff631be066a5c98a2
skyria10 <60612406+skyria10@users.noreply.github.com>
2021-11-30 22:36:23 +0200

    Update index.js

--- client/user/src/componets/Views/Card/AddNewCard/index.js
+++ client/user/src/componets/Views/Card/AddNewCard/index.js
@@ -15,6 +15,9 @@ import {
     formState,
     form,
     Link,
+    FormLabel,
+    Textarea
+
   } from "@chakra-ui/react";
   import { FaCar,FaCcMastercard,FaPlus,FaCcVisa } from "react-icons/fa";
   import {
@@ -29,15 +32,56 @@ import {
     return (
           <Stack width="-webkit-fill-available">
           <Heading paddingTop="10" alignSelf="center">Card Details</Heading>
-          <Stack width="min-content" alignSelf="center">
-           <Button as={RouterLink} to="/card" backgroundColor="gray.300" borderRadius="15" width="full" textAlign="center">
-            <HStack >
-              <FaPlus/>
-              <Text>
-                ADD CARD
-              </Text>
-            </HStack>
-          </Button>
+          <Stack width="250px" alignSelf="center">
+          <form paddingTop="10" alignSelf="center" >
+        <FormLabel>Card 16-digit Number</FormLabel>
+        <Input
+          focusBorderColor="#16DABF"
+          color="#c1c1c1"
+          type="text"
+          backgroundColor="gray.200"
+          textColor="black"
+          name="name"
+        />
+        <FormLabel>Card Holders Name</FormLabel>
+        <Input
+          focusBorderColor="#16DABF"
+          color="#c1c1c1"
+          type="text"
+          backgroundColor="gray.200"
+          textColor="black"
+          name="phone"
+        />
+        <FormLabel>CVV</FormLabel>
+        <Input
+          focusBorderColor="#16DABF"
+          color="#c1c1c1"
+          type="int"
+          backgroundColor="gray.200"
+          textColor="black"
+          name="email"
+        />
+        <FormLabel>Expiration Date</FormLabel>
+        <Input
+          focusBorderColor="#16DABF"
+          color="#c1c1c1"
+          type="date"
+          backgroundColor="gray.200"
+          textColor="black"
+          name="email"
+        />
+        <Stack paddingTop="10" alignSelf="center" alignContent="center">
+        <Button as={RouterLink} to="/card" backgroundColor="gray.300" borderRadius="15" width="full" textAlign="center">
+              <HStack >
+                <FaPlus/>
+                <Text>
+                  ADD CARD
+                </Text>
+              </HStack>
+            </Button>
+        </Stack>
+      </form>
+            
           </Stack>
           </Stack>
     );
